98eded05557264c56552ea35509dc56782ccdf04
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-21 22:43:37 +0530

    Create MinorColor.c

---
+++ MinorColor.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
