ca847c994690da39a4c16df4e0c2e49703a1d956
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-21 23:08:42 +0530

    Update MinorColor.c

--- MinorColor.c
+++ MinorColor.c
@@ -1,15 +1,9 @@
 #include <assert.h>
 
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
+
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
