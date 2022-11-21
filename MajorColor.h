486254026507c36a0d320de18ef3ee820ffdd170
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-21 23:42:06 +0530

    Update MajorColor.h

--- MajorColor.h
+++ MajorColor.h
@@ -3,5 +3,3 @@ enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
