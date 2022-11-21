82739b8a282c066d4a2a8d48371d32ab65a57f04
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-21 23:07:36 +0530

    Update MajorColor.c

--- MajorColor.c
+++ MajorColor.c
@@ -11,8 +11,3 @@ int numberOfMajorColors =
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
