ad487c3c5959b3975019c994e7cef6e1f9a303cc
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-21 23:09:35 +0530

    Update MajorColor.c

--- MajorColor.c
+++ MajorColor.c
@@ -1,13 +1,11 @@
-#include <assert.h>
+//#include <assert.h>
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
