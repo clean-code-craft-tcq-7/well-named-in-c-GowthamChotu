6421f9e7ae289096dc6d1398bbf88d077902b80d
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-21 23:12:41 +0530

    Update MajorColor.c

--- MajorColor.c
+++ MajorColor.c
@@ -7,5 +7,3 @@ const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
-
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
