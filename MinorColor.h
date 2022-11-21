cdd9629562a972cdb9738b6aa0b97c3b4711a658
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-21 23:42:27 +0530

    Update MinorColor.h

--- MinorColor.h
+++ MinorColor.h
@@ -2,6 +2,4 @@ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
