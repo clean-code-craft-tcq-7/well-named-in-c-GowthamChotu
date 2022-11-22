cbf3df958fd7f724b279091607e1681ed7f2f6e2
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-22 23:11:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -13,16 +13,18 @@ int main() {
     testPairToNumber(VIOLET, SLATE, 25);
     
     
+    int k=0;
     for(int i=1; i<=numberOfMajorColors;i++)
     {
         for(int j=1; j<=numberOfMinorColors; j++)
         {
+            k++;
             //ColorPair colorPair = GetColorFromPairNumber(i);
             //char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
             //ColorPairToString(&colorPair, colorPairNames);
             //printf("Pair Number %d is %s\n", i+j,colorPairNames);
             //printf("Pair Number %d is %s and %s\n" i, 
-            printf("Pair Number %d is ",i);
+            printf("Pair Number %d is ",k);
             testNumberToPair(4, WHITE, BROWN);
             
             
