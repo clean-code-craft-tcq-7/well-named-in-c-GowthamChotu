b752ec3f121cfba12ce7efd327fea25e0e229abb
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-22 23:59:00 +0530

    Update main.c

--- main.c
+++ main.c
@@ -12,25 +12,9 @@ int main() {
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
     
+    printf("Details of Color Coding Are As Follows:\n");
+    Print_Reference_Manual();
     
-    int k=0;
-    for(int i=1; i<=25;i++)
-    {
-        //for(int j=1; j<=numberOfMinorColors; j++)
-        //{
-            k++;
-            //printf("Pair Number %d is ",k);
-            ColorPair colorPair = GetColorFromPairNumber(i);
-            char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-            ColorPairToString(&colorPair, colorPairNames);
-            printf("Pair Number %d is %s\n", k, colorPairNames);
-            //testNumberToPair(i, (enum MajorColor)(i), (enum MinorColor)(i));
-            //testPairToNumber((enum MajorColor)(i), (enum MinorColor)(j), 12);
-            
-        //}
-        
-    }
-
     
     return 0;
 }
