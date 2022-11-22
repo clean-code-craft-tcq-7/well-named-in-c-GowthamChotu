c5410dbf876fd01a04dcfa0a21bc62fa17441cbd
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-22 14:01:16 +0530

    Update Major_and_Minor.h

--- Major_and_Minor.h
+++ Major_and_Minor.h
@@ -1,5 +1,16 @@
-#include "MajorColor.h"
-#include "MinorColor.h"
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
