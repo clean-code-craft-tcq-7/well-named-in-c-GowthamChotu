4985e0863accb68e124eec58c88499a775a9b851
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-22 14:00:34 +0530

    Delete MinorColor.h

--- MinorColor.h
+++
@@ -1,5 +0,0 @@
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
