11b3f51da42d6542a29944b674364ec73ba396f6
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-22 14:00:24 +0530

    Delete MajorColor.h

--- MajorColor.h
+++
@@ -1,5 +0,0 @@
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
