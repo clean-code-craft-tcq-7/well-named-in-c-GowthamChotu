53e665d855f01e2509c6e25b4ea0477cf9392cd5
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-11-22 22:57:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main() {
             //printf("Pair Number %d is %s\n", i+j,colorPairNames);
             //printf("Pair Number %d is %s and %s\n" i, 
             
-            testNumberToPair(i, WHITE, BROWN);
+            testNumberToPair(6, WHITE, BROWN);
             
             
         }
